c598debe7d8a1bedd07e63c7c9b97c83a2402220
Googly-Boogly <mainalextaylor@gmail.com>
2023-10-07 17:23:35 -0500

    Add files via upload

---
+++ data_handling.py
@@ -0,0 +1,77 @@
+import json
+
+
+def run_txt_to_data(line_number):
+    """
+    takes in the line number and returns the list of lists as lists of lists
+    :param line_number: (int) of line number to retrieve
+    :return: (list) returns the list of lists of the data for a specifc line
+    """
+    filename = 'data.txt'
+    data = txt_file_to_data(filename, line_number)
+
+    if data is not None:
+        # print(data)
+        # print(len(data))
+        return data
+    else:
+        print(f"Line {line_number} not found or has invalid JSON format.")
+
+
+def txt_file_to_data(filename, line_number):
+    """
+    Read a specific line from a text file containing JSON data.
+
+    Args:
+        filename (str): The name of the text file to read.
+        line_number (int): The line number to read (1-based index).
+
+    Returns:
+        dict: The parsed JSON data from the specified line.
+        None: if invalid json format or line number is out of range
+
+    """
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        if 1 <= line_number <= len(lines):
+            line_content = lines[line_number - 1].strip()
+            try:
+                data = json.loads(line_content)
+                return data
+            except json.JSONDecodeError:
+                return None  # Invalid JSON format
+        else:
+            return None  # Line number is out of range
+
+
+def count_lines_in_file(filename='data.txt'):
+    """
+    Count the number of lines in a text file.
+
+    Args:
+        filename (str): The name of the text file to count lines in.
+
+    Returns:
+        int: The number of lines in the file.
+    """
+    with open(filename, 'r') as file:
+        line_count = sum(1 for line in file)
+    return line_count
+
+
+def delete_last_n_lines(filename='data.txt', n=3):
+    """
+    Delete the last n (3) lines from a text file.
+
+    Args:
+        filename (str): The name of the text file to modify.
+        n (int): The number of lines to delete from the end of the file. Default is 3.
+    """
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+
+    keep_lines = lines[:-n]
+
+    with open(filename, 'w') as file:
+        file.writelines(keep_lines)
+
---
+++ main.py
@@ -0,0 +1,152 @@
+from datetime import datetime
+import cv2
+from handtrackingmodule import handTracker
+import random
+import json
+from data_handling import run_txt_to_data, count_lines_in_file, delete_last_n_lines
+
+
+def main_loop_tests():
+    """
+    This function will run a loop that asks the user to change their hand position and will record the data of the user doing it
+    ALSO this function gets your camera and runs some deep learning to figure out the 21 points of your hand
+    the points go as follows.
+    0: wrist
+    1-4 Thumb (from base to tip so 1 is the base of the thumb and 4 is the tip of the thumb)
+    5-8 index finger
+    9-12 middle finger
+    13-16 ring finger
+    17-20 little finger (winter is coming)
+    :param: NA
+    :return:
+    """
+    cap = cv2.VideoCapture(0)
+    tracker = handTracker()
+
+    desired_fps = 10
+
+    frame_accumulator = []  # To store frames for the last 3 seconds
+    start_time = datetime.now()  # Start time of the 5-second interval
+    start_time_frames = datetime.now()
+    text = ''
+    font_color = (0,0,0)
+    while True:
+
+        frame_start_time = datetime.now()
+
+        success, image = cap.read()
+        image = tracker.handsFinder(image)
+        lmList = tracker.positionFinder(image)
+
+        if len(lmList) != 0:
+            if lmList[20][1] < 30:
+                print('Left')
+            elif lmList[4][1] > 600:
+                print('Right')
+
+        image = add_text_to_image(image=image, text=text, font_color=font_color)
+        cv2.imshow("Video", image)
+
+        frame_accumulator.append(lmList)
+
+        # Calculate the time taken for processing and display
+        # frame_end_time = datetime.now()
+        # elapsed_time = frame_end_time - frame_start_time
+        # print(f"Frame processing time: {elapsed_time}")
+
+        # Check if 5 seconds have elapsed
+        current_time = datetime.now()
+        if (current_time - start_time).total_seconds() >= 5:
+            start_time = current_time
+            start_time_frames = current_time
+            frame_accumulator = []
+            rand = get_random()
+            text = rand[0]
+            font_color = rand[1]
+
+        if (current_time - start_time_frames).total_seconds() >= 3:
+            if not text == '':
+                frames_to_send = frame_accumulator
+                store_data(frames_to_send)
+                text = ''
+
+        key = cv2.waitKey(1000 // desired_fps)
+
+        if key == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+def store_data(data, filename='data.txt'):
+    """
+    Append a list of lists to a text file where each line represents one inner list.
+
+    Args:
+        filename (str): The name of the text file to save or append the data to.
+        data (list): A list of 19 lists, each containing 3 integers.
+    """
+    with open(filename, 'a') as file:  # Use 'a' for append mode
+        # Convert the data to a JSON string and write it to the file
+        json_data = json.dumps(data)
+        file.write(json_data + '\n')
+
+
+def add_text_to_image(image, text,font_color, position=(100, 300), font=cv2.FONT_HERSHEY_SIMPLEX, font_scale=1, font_thickness=2):
+    """
+    Adds text to an image and returns the modified image.
+
+    Args:
+        image (numpy.ndarray): The input image.
+        text (str): The text to be added.
+        position (tuple): The (x, y) coordinates where the text will be placed.
+        font (int): The font type (e.g., cv2.FONT_HERSHEY_SIMPLEX).
+        font_scale (float): The font scale factor.
+        font_color (tuple): The font color in BGR format.
+        font_thickness (int): The thickness of the text.
+
+    Returns:
+        numpy.ndarray: The modified image with text.
+    """
+
+    output_image = image.copy()
+    cv2.putText(output_image, text, position, font, font_scale, font_color, font_thickness)
+    return output_image
+
+
+def get_random():
+    """
+    randomly returns the text hand down, hand up, hand steady and the font color
+    :return: text and font color
+    """
+    random_num = random.randint(0, 2)
+    # 0 will be point hand down
+    # 1 will be point hand up
+    # 2 will be keep hand steady
+    if random_num == 0:
+        text = 'HAND DOWN'
+        font_color = (255, 255, 0)
+    elif random_num == 1:
+        text = 'HAND UP'
+        font_color = (255, 0, 255)
+    else:
+        text = 'KEEP HAND STEADY'
+        font_color = (0, 255, 255)
+    return text, font_color
+
+
+def start_function():
+
+    x = input('Hello, would you like to (gather) data or (view) data:')
+    if x.lower() == 'gather':
+        print('Press q to exit out')
+        delete_last_n_lines() # deletes last 3 lines of the txt file incase when shutting off you moved your hand weird
+        main_loop_tests()
+    if x.lower() == 'view':
+        for x in range(count_lines_in_file()):
+            data = run_txt_to_data(x + 1)
+            print(data)
+
+
+if __name__ == '__main__':
+    start_function()
